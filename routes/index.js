3bc83bfb9edff943ee65e4f8d51a315648326f33
alinperez <alinadrian2391@gmail.com>
2022-01-24 13:00:44 -0500

    Add Signature

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Carbonell Furniture' });
+  res.render('index', { title: 'Carbonell Furniture',designer: 'Yuliet Carbonell' });
 });
 
 module.exports = router;
